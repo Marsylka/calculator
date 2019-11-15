5df94e5c7c40af4a9425aa09ed522bf3ae4e8a71
Marsylka <mars.nightmares888@gmail.com>
2019-11-15 14:12:44 +0000

    sadasd

--- js/index.js
+++ js/index.js
@@ -1,16 +1,41 @@
+
 const display = document.querySelector('.calculator .display');
-document.querySelectorAll('.number button, .operators button')
-    .forEach(button => button.addEventListener('click', numberOperatorsPressed));
+document.querySelectorAll('.numbers button, .operators button')
+.forEach( button => button.addEventListener('click', numberOperatorsPressed));
 function numberOperatorsPressed(revision) {
-    const btnText = revision.target.innetText;
+    
+    const btnText = revision.target.innerText;
     display.value += btnText;
+
 }
 document.querySelector('.calculator .equal')
     .addEventListener('click', equalPressed);
-function eqPressed() {
+function equalPressed() {
+    if (eval(display.value) === Infinity) {
+    display.value = "Stop this!!! you crash me("
+    }else {
     display.value = eval(display.value);
 }
+}
+
 document.querySelector('.calculator .clearToken')
     .addEventListener('click', clearTokenPressed);
 function clearTokenPressed() {
+   display.value = display.value.slice(0, -1);
 }
+document.querySelector('.calculator .clearAll')
+    .addEventListener('click', clearAllPressed);
+function clearAllPressed() {
+    display.value = "";
+}
+document.querySelector('.operators button' )
+.addEventListener('click', doubleOperatorsPressed);
+function doubleOperatorsPressed(){
+    if (display.value.slice(-1) = '-' || '+'){
+        
+
+     
+    }
+}
+
+
